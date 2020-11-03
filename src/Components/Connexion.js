2b1fe8edfdb8af6204f4a8b04659a2973ad54967
Vincent Chapront <vincent.chapront@gmail.com>
2020-11-03 16:42:56 +0100

    Composent de connexion

---
+++ src/Components/Connexion.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from "react";
+
+class Connexion extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>Page de connexion</p>
+      </Fragment>
+    );
+  }
+}
+
+export default Connexion;
