44b696a2f7bc353d0991eb9dfe2bdf3e7624e6e6
Vincent Chapront <vincent.chapront@gmail.com>
2020-11-04 09:34:52 +0100

    Construction du formulaire de connexion

--- src/Components/Connexion.js
+++ src/Components/Connexion.js
@@ -1,13 +1,38 @@
 import React, { Component, Fragment } from "react";
 
 class Connexion extends Component {
-  render() {
-    return (
-      <Fragment>
-        <p>Page de connexion</p>
-      </Fragment>
-    );
-  }
+
+    state={name:""}
+    change=event=>{
+        const { name , value }= event.target
+        this.setState({ [name]:value})
+    }
+
+    submit=event=>{
+        event.preventDefault()
+        console.log("Submit : Nom : " + this.state.name)
+    }
+
+    render() {
+        return (
+        <Fragment>
+            <p>Page de connexion</p>
+            <form onSubmit={this.submit}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Nom</td>
+                            <td><input name="name" type="text" onChange={this.change}/></td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2"><button type="submit">Connexion</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </Fragment>
+        );
+    }
 }
 
 export default Connexion;
