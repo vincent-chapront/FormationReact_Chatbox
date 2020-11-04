f183ac8399f249dcd1384d035db0e4de7f57c1b5
Vincent Chapront <vincent.chapront@gmail.com>
2020-11-04 09:12:13 +0100

    Routing

---
+++ src/Components/Page404.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+class Page404 extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>404 : page non trouvée</p>
+      </Fragment>
+    );
+  }
+}
+
+export default Page404;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Connexion from "./Components/Connexion";
+import Page404 from "./Components/Page404";
+import App from "./App";
 import * as serviceWorker from './serviceWorker'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<Connexion />, document.getElementById('root'))
+const Root=()=>(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Connexion}/>
+            <Route path="/app" component={App}/>
+            <Route component={Page404}/>
+        </Switch>
+    </BrowserRouter>
+)
+
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
